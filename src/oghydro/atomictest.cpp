29ebf9b9e611c62f82ab7e2078bc0a5f0c14563f
Meg Blackburn <mgb27@login01.hpc.st-andrews.ac.uk>
2026-08-06 14:01:16 +0100

    remove unnecessary directories

--- src/oghydro/atomictest.cpp
+++
@@ -1,8 +0,0 @@
-#include <atomic>
- #include <new>
- int main(int, char**){
- double a = 1.;
- std::atomic<double> *atom = new(&a) std::atomic<double>;
- double old = *atom;
- while (!atom->compare_exchange_weak(old, old + 1.)) {}
- return 0;}
--- src/oghydro/posixtest.cpp
+++
@@ -1,2 +0,0 @@
-#include <unistd.h>
-int main(int, char**){ return 0; }
